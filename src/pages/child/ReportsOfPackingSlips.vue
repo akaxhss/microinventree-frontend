27a7d385bff7ee7f2e34916de7ca8925c9925f65
Akash Satheesh <satheeshakash07@gmail.com>
2025-11-02 14:14:33 +0530

    function date time

--- src/pages/child/ReportsOfPackingSlips.vue
+++ src/pages/child/ReportsOfPackingSlips.vue
@@ -530,7 +530,7 @@ const formatDate = (dateString) => {
 };
 
 const formatDateTime = (dateString) => {
-  return new Date(dateString).toLocaleString();
+  return new Date(dateString).toLocaleString('en-GB');
 };
 
 const groupByProduct = (lines) => {
